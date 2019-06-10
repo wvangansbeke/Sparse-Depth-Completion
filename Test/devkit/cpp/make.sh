37afa879719297abbf19abee74955db6c33cfd85
wvangansbeke <woutervangansbeke@hotmail.com>
2019-06-10 21:48:18 +0200

    Update repo

--- Test/devkit/cpp/make.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "==========================================================================="
-g++ -O3 -DNDEBUG -Wno-unused-result -o evaluate_depth evaluate_depth.cpp -lpng
-echo "Built evaluate_depth."
--- Test/devkit/matlab/depth_read.m
+++
@@ -1,8 +0,0 @@
-function D = depth_read (filename)
-% loads depth map D from png file
-% for details see readme.txt
-
-I = imread(filename);
-D = double(I)/256;
-D(I==0) = -1;
-
